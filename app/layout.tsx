44870ebbbe9717efefde0fbec45ef0a1c3025db8
Matthew Collins <matthewcollins@Matthews-MacBook-Pro.local>
2025-03-04 19:45:56 -0500

    more revisional updates to web application

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { cn } from "@/lib/utils"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 import type React from "react" // Import React
+
 
 const inter = Inter({ subsets: ["latin"] })
 
